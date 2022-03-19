410980ed5b4f7395b3e7b5fa3b45b4401484b388
Liz Andersen <landersen801@gmail.com>
2022-03-19 15:55:32 -0600

    first commit

---
+++ week-4/index.js
@@ -0,0 +1,56 @@
+console.log("Here is my first console log!");
+
+let bananaColor = 'green'; 
+const isBananaTree = 'yellow';
+
+console.log("Here is my brain frying 'Liz says that'");
+
+console.log(1);
+console.log(-1);
+console.log(1.5);
+
+let myBoolean = true;
+let isWarmOut = false;
+
+let dinnerTonight;
+let dinnerTomorrowNight = undefined;
+
+let anything = null;
+
+
+let dog = {
+    name: 'Duke',
+    breed: 'Dachshund',
+    color: 'Brown',
+    gender: 'male',
+    age: 7,
+    isHyper: true,
+    favoriteFoods: ["my cat"]
+}
+
+console.log(dog);
+console.log(dog.name);
+
+let color = "blue";
+console.log(typeof color);
+color = false;
+console.log(typeof color);
+
+    
+function greeting() {
+    console.log("hello!");
+}
+greeting();
+
+function greeting2(greeting) {
+    console.log(greeting);
+}
+
+greeting2("Hola");
+
+function greeting3(greeting, firstName, lastName) {
+  console.log(greeting, firstName, lastName);
+}
+
+greeting3("I am tired", "says Liz", "Andersen");
+
